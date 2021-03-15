80a1f9232e756d340f41d0c98f447a0fb3f84ed3
Euen <euen@catalx.io>
2021-03-15 19:00:04 -0300

    Add price chart

--- lib/poeticoins_web/live/product_component.ex
+++ lib/poeticoins_web/live/product_component.ex
@@ -3,7 +3,14 @@ defmodule PoeticoinsWeb.ProductComponent do
   import PoeticoinsWeb.ProductHelpers
 
   def update(%{trade: trade} = _assigns, socket) when not is_nil(trade) do
-    socket = assign(socket, :trade, trade)
+    product_id = to_string(trade.product)
+    event_name = "new-trade:#{product_id}"
+
+    socket =
+      socket
+      |> assign(:trade, trade)
+      |> push_event(event_name, to_event(trade))
+
     {:ok, socket}
   end
 
@@ -43,6 +50,16 @@ defmodule PoeticoinsWeb.ProductComponent do
               <%= @trade.price %>
               <%= fiat_character(@product) %>
           </div>
+      </div>
+
+      <div class="chart-component">
+        <div phx-hook="Chart"
+            id="product-chart-<%= to_string(@product) %>"
+            data-product-id="<%= to_string(@product) %>"
+            phx-update="ignore"
+            >
+          <div class="chart-container"></div>
+        </div>
       </div>
 
       <div class="exchange-name">
@@ -86,5 +103,13 @@ defmodule PoeticoinsWeb.ProductComponent do
       </div>
     </div>
     """
+  end
+
+  defp to_event(trade) do
+    %{
+      traded_at: DateTime.to_unix(trade.traded_at, :millisecond),
+      price: trade.price,
+      volume: trade.volume
+    }
   end
 end
